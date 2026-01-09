c4bbddc1e96a201b583c9a7836b450cee345c335
jeancmr <jean.madiedo08@gmail.com>
2026-01-09 16:00:48 -0500

    first commit

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+
+bootstrap()
+  .then(() =>
+    console.log(`Server is running on port ${process.env.PORT ?? 3000}`),
+  )
+  .catch(console.error);
