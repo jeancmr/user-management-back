90216e7052070977a92aecb4324b243ccbb3435a
jeancmr <jean.madiedo08@gmail.com>
2026-01-09 16:14:17 -0500

    feat: add user module and add user entity

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,14 @@
+import { UserPlan } from '../enums/user-plan.enum';
+import { UserRole } from '../enums/user-role.enum';
+import { UserStatus } from '../enums/user-status.enum';
+
+export class User {
+  id: string;
+  name: string;
+  role: UserRole;
+  status: UserStatus;
+  plan: UserPlan;
+  lastLogin: Date;
+  createdAt: Date;
+  updatedAt: Date;
+}
